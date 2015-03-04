ffa467dea9fd9c0f4b71bc2c6a1e1190929a88ab
dhaniwid <real.ramadhani.widodo@gmail.com>
2015-03-04 18:20:41 +0700

    commit deui wae

--- app/views/confirmation-booking.blade.php
+++ app/views/confirmation-booking.blade.php
@@ -85,7 +85,6 @@
           </div>
           <div class="col-lg-1" style='padding-top: 8px'><span>:</span></div>
           <div class='col-lg-5' style='padding-top: 8px'>
-              <!--@currency($reservation->total,'IDR')-->
               {{$reservation->total}}
           </div>
         </div>
